e1449f8ce4a18c60a6f5635019f073e8c9cb5f90
markdavidmc0 <mark@dotmodus.com>
2020-08-17 22:46:00 +0200

    Add options to media page model.

--- rock_site/home/models.py
+++ rock_site/home/models.py
@@ -1,7 +1,36 @@
 from django.db import models  # noqa: F401
 
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
     pass
+
+
+class MediaPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('carousel', blocks.StreamBlock([
+            ('first_image', ImageChooserBlock()),
+            ('second_image', ImageChooserBlock()),
+            ('third_image', ImageChooserBlock()),
+            ('fourth_image', ImageChooserBlock()),
+            ('fifth_image', ImageChooserBlock()),
+        ], icon='cogs'
+        )),
+        ('switcher', blocks.StructBlock([
+            ('title', blocks.CharBlock()),
+            ('description', blocks.RichTextBlock()),
+        ], icon='user'))
+
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body')
+    ]
